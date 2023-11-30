f4f945971c1e2a8f291a3402a579bbba0c009e46
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-11-30 15:42:57 +0300

    modified home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,6 +11,8 @@ function Home() {
       <div>
         <h2>Welcome To Schroders investments</h2>
         <p>Schroders investments is serving the world with its belief in Online marketing Strategies to help achieve great financial success by joining a world class team of investors.Build your own dedicated team</p>
+      
+      <h3>START TODAY START NOW</h3>
       </div>
     </div>
   )
